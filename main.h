55339222beac5fb332bc063919cfc5b7ae49efbd
lishadeve <erimalisha7@gmail.com>
2022-10-20 17:06:26 -0700

    _printf

--- main.h
+++ main.h
@@ -4,6 +4,7 @@
 #include <stdio.h>
 #include <unist.h>
 
+#define UNUSED(x) (void)(x)
 #define BUFFER_SIZE 1024
 
 /**
@@ -21,14 +22,21 @@ typedef struct fmt fmt_t;
 
 
 int _printf(const char *format, ...);
-int handle_print(const char *fmt, int *i,
-		va_list list, char buffer[], int flags, int width, int precision, int size);
+int handle_print(const char *fmt, int *index, va_list list,
+		char buffer[], int flags, int width, int precision, int size);
 
-
+/* FUNCTIONS */
 int print_char(va_list types, char buffer[],
 		int flags, int width, int precision, int size);
 
 int handle_write_char(char c, char buffer[],
 		int flags, int width, int precision, int size);
+
+int get_flags(const char *format, int *index);
+int get_width(const char *format, int *index, va_list list);
+int get_precision(const char *format, int *index, va_list list);
+int get_size(const char *format, int *index);
+
+int is_printable(char);
 
 #endif
